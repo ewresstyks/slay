a46acd7532db100983e00f0b7536dc36036b9672
Boyko_rg21 <piterzzfka@gmail.com>
2025-11-05 09:37:48 +0200

    slay update tokenn

--- bot.py
+++ bot.py
@@ -24,9 +24,10 @@ url = f"https://api.telegram.org/bot{bot_key}/"  # don't forget to change the to
 
 def last_update(request):
     response = requests.get(request + 'getUpdates')
-    print(response)
+    # TODO: Uncomment just for local testing
+    # print(response)
     response = response.json()
-    print(response)
+    # print(response)
     results = response['result']
     total_updates = len(results) - 1
     return results[total_updates]
@@ -49,31 +50,35 @@ def send_message(chat, text):
 
 
 def main():
-    update_id = last_update(url)['update_id']
-    while True:
-        # pythonanywhere
-        time.sleep(3)
-        update = last_update(url)
-        if update_id == update['update_id']:
-            if get_message_text(update).lower() == 'hi' or get_message_text(
-                    update).lower() == 'hello' or get_message_text(update).lower() == 'hey':
-                send_message(get_chat_id(update), 'Greetings! Type "Dice" to roll the dice!')
-            elif get_message_text(update).lower() == 'csc31':
-                send_message(get_chat_id(update), 'Python')
-            elif get_message_text(update).lower() == 'python':
-                send_message(get_chat_id(update), 'version 3.14')
-            elif get_message_text(update).lower() == 'dice':
-                _1 = random.randint(1, 6)
-                _2 = random.randint(1, 6)
-                send_message(get_chat_id(update),
-                             'You have ' + str(_1) + ' and ' + str(_2) + '!\nYour result is ' + str(_1 + _2) + '!')
-            else:
-                send_message(get_chat_id(update), 'Sorry, I don\'t understand you :(')
-            update_id += 1
+    try:
+        update_id = last_update(url)['update_id']
+        while True:
+            # pythonanywhere
+            time.sleep(3)
+            update = last_update(url)
+            if update_id == update['update_id']:
+                if get_message_text(update).lower() == 'hi' or get_message_text(
+                        update).lower() == 'hello' or get_message_text(update).lower() == 'hey':
+                    send_message(get_chat_id(update), 'Greetings! Type "Dice" to roll the dice!')
+                elif get_message_text(update).lower() == 'qa24':
+                    send_message(get_chat_id(update), 'Python')
+                elif get_message_text(update).lower() == 'gin':
+                    send_message(get_chat_id(update), 'Finish')
+                    break
+                elif get_message_text(update).lower() == 'python':
+                    send_message(get_chat_id(update), 'version 3.10')
+                elif get_message_text(update).lower() == 'dice':
+                    _1 = random.randint(1, 6)
+                    _2 = random.randint(1, 6)
+                    send_message(get_chat_id(update),
+                                 'You have ' + str(_1) + ' and ' + str(_2) + '!\nYour result is ' + str(_1 + _2) + '!')
+                else:
+                    send_message(get_chat_id(update), 'Sorry, I don\'t understand you :(')
+                update_id += 1
+    except KeyboardInterrupt:
+        print('\nБот зупинено')
 
 
 # print(__name__)
 if __name__ == '__main__':
     main()
-# print(__name__)
-# print('HELLO') #При подключении файла как бибилиотеки import bot, в другой .py файл проекта, этот код будет запускатся при включении того, другого файла
