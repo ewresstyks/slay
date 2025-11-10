4494a8e42312a39b6c539a024b3f2fc1cfaacd4b
Boyko_rg21 <piterzzfka@gmail.com>
2025-11-10 10:16:29 +0200

    mememe

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,2 +1,13 @@
+from calculator import calculate_expression
+
+def test_calculate_addition():
+    assert calculate_expression('1 + 2 + 3') == '6'
+
+def test_calculate_subtraction():
+    assert calculate_expression('2 - 3') == '-1'
+
 def test_smoke():
+    assert True
+
+def test_smoke1():
     assert True
